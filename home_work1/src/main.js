4ae38d3b431bbd42750a074e0a594f8a894e7949
COMPUTER\Dima <berkutov.dmitriy@stemsc.com>
2017-05-14 22:51:57 +0300

    home 1

---
+++ home_work1/src/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Dima on 14.05.2017.
+ */
+
+var nikita = {
+    name: 'Nikita',
+    number: 380959848893,
+    knowledge : ['Angular', 'React'],
+    nikita_info: {
+        skype: 'nikita5224',
+        mail: 'nikita@qqq.gmail.com'
+    },
+    confirmation: true
+};
+
+console.log(nikita);
