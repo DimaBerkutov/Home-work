d70cc4fc889badddcb2eb6ec86746e0ed8a432db
COMPUTER\Dima <berkutov.dmitriy@stemsc.com>
2017-05-25 23:07:27 +0300

    task 4

--- home_work4/src/main.js
+++ home_work4/src/main.js
@@ -8,27 +8,27 @@
  и возвращает массив содержащий числа между первым числом и вторым числом;
  */
 
-// function numbersBetween(valueBetween1, valueBetween2) {
-//     if(valueBetween2 < valueBetween1){
-//         return ('error, value2 < value1');
-//     }
-//     if (!isNaN(valueBetween1) && !isNaN(valueBetween2)) {
-//         let arrBetween = [];
-//         valueBetween1++;
-//         for (let i = valueBetween1; i < valueBetween2; i++) {
-//             arrBetween.push(i);
-//         }
-//         return arrBetween;
-//     }
-//     else {
-//         return ('error');
-//     }
-// }
-//
-// console.log(numbersBetween(1, 5)); // 2,3,4
-// console.log(numbersBetween(3, 6)); // 4,5
-// console.log(numbersBetween(12, 'gf')); // 13,14
-// console.log(numbersBetween(55, 15)); // 11, 12, 13, 14
+function numbersBetween(valueBetween1, valueBetween2) {
+    if(valueBetween2 < valueBetween1){
+        return ('error, value2 < value1');
+    }
+    if (!isNaN(valueBetween1) && !isNaN(valueBetween2)) {
+        let arrBetween = [];
+        valueBetween1++;
+        for (let i = valueBetween1; i < valueBetween2; i++) {
+            arrBetween.push(i);
+        }
+        return arrBetween;
+    }
+    else {
+        return ('error');
+    }
+}
+
+console.log('task1 ',numbersBetween(1, 5)); // 2,3,4
+console.log('task1 ',numbersBetween(3, 6)); // 4,5
+console.log('task1 ',numbersBetween(12, 'gf')); // 13,14
+console.log('task1 ',numbersBetween(55, 15)); // 11, 12, 13, 14
 
 
 
@@ -43,26 +43,31 @@
 Если передаваемое число не кратно 3 или 5, то вернуть указанный параметр
 */
 
-// for(let i = 0; i <= 100; i++){
-//     let arrValue = fizzBuzz(i);
-//     // fizzBuzz(arrValue);
-//     console.log(i, arrValue);
-// }
-// function fizzBuzz(num) {
-//     if(!isNaN(num) && (num % 5) == 0 && (num % 3) == 0){
-//         return('FizzBuzz3,5');
-//     }
-//     if(!isNaN(num) && (num % 3) == 0){
-//         return('Fizz3');
-//     }
-//     if(!isNaN(num) && (num % 5) == 0){
-//         return('Buzz5');
-//     }
-//     return num;
-// }
+function someFunction (someValue) {
+    if(!isNaN(someValue)){
+        let arrValue = [];
+        for(let i = 0; i <= someValue; i++){
+            arrValue.push(fizzBuzz(i));
+        }
+        return fizzBuzz(arrValue);
+    }
+    return someValue;
+}
+function fizzBuzz(num) {
+    if(num && (num % 5) == 0 && (num % 3) == 0){
+        return('FizzBuzz3,5');
+    }
+    if(num && (num % 3) == 0){
+        return('Fizz3');
+    }
+    if(num && (num % 5) == 0){
+        return('Buzz5');
+    }
+    return num;
+}
 
-// console.log(fizzBuzzStart(100)); // 1
-// console.log(fizzBuzzStart('fdsadsfdafda')); // 'Fizz'
+console.log('task2 ', someFunction(100)); // 1
+console.log('task2 ', someFunction('fdsadsfdafda')); // 'Fizz'
 
 
 
@@ -80,5 +85,61 @@ function task3Arr(someValue) {
     return arrType;
 }
 
-console.log(arrOld); // 'Fizz'
-console.log(task3Arr(arrOld)); // 'Fizz'
+console.log('task3 ',arrOld); // 'Fizz'
+console.log('task3 ',task3Arr(arrOld)); // 'Fizz'
+
+
+
+
+/*
+ @@SUPER@@. Вам дан массив array, содержащий внутри объекты.
+ Напишите функцию которая внутри цикла проходится по каждому элементу массива
+ И проверяет какой тип данных содержит свойство age, если тип данных NaN,
+ тогда добавляет данному объекту свойство unknownAge: true
+
+ На основании нового массива, создайте новую функцию, которая вернет новый массив
+ содержащий все объекты содержащие свойство unknownAge:true
+ */
+
+let array = Array.from({ length: 35 }).map(
+    (value, index) => (index % 2 ? { age: index + 2 } : { age: NaN })
+);
+
+function ageFunction() {
+    for(let i = 0; i < array.length; i++) {
+        for(let key in array[i]){
+            let arrKey = array[i];
+            // console.log('key = ', key);
+            if (isNaN(arrKey[key])) {
+                arrKey.unknownAge = true;
+            }
+        }
+    }
+    // newMassFunction(array);
+    return newMassFunction(array);
+}
+function newMassFunction(incomeArr) {
+    var newArr = [];
+    for(let i = 0; i < array.length; i++) {
+        if('unknownAge' in incomeArr[i] && incomeArr[i]['unknownAge']){
+            newArr.push(incomeArr[i]);
+        }
+    }
+    return newArr;
+}
+
+
+console.log('task4 ', 'ageFunction =', ageFunction());
+
+console.log('task4 ', array); // [ {age:NaN}, {age:3}, {age:NaN}, {age:5}, {age:NaN}, {age:7} ....]
+console.log('task4 ', array.length); // 35
+
+
+
+// Unnecessary values
+// let arr = ['privet', 12, {}, [1, 2]];
+// let arr2 = ['privet', 12, {}, [1, 2]];
+//
+// function returnArr(argument) {}
+// returnArr(arr);
+// returnArr(arr2);
