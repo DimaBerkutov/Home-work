9887133bcc15c9af33fb4d7cc7dce3a647a33839
Berkutov.D <berkutov.dmitriy@stemsc.com>
2017-06-20 11:45:10 +0300

    home11

--- home_work11/src/main.js
+++ home_work11/src/main.js
@@ -16,20 +16,20 @@
  // - Гости могут зарегистрироваться в базе данных
 
 
-// let user = {};
-// let verifiedUser = {
-//     adminMAil: 'qqq@qqq.com'
-// };
-// let gest = {};
-// let base = {
-//     user: 'Dima',
-//     user2: 'Nikita',
-//     databasePaass: 'qeeqe14512fwf'
-// };
-// user.__proto__= verifiedUser;
-// verifiedUser.__proto__= base.databasePaass;
-// console.log(user.adminMAil);
-// console.log(user.databasePaass);
+let user = {};
+let verifiedUser = {
+    adminMAil: 'qqq@qqq.com'
+};
+let gest = {};
+let base = {
+    user: 'Dima',
+    user2: 'Nikita',
+    databasePaass: 'qeeqe14512fwf'
+};
+user.__proto__= verifiedUser;
+verifiedUser.__proto__= base.databasePaass;
+console.log(user.adminMAil);
+console.log(user.databasePaass);
 
 
 
@@ -41,135 +41,135 @@
 // return-object-this - так же как и предидуший, value заходит в user, а там name это "Василий"
 
 // calculator
-// var calculator = {
-//     val1: '0',
-//     val2: '0',
-//     read(){
-//         this.val1 = prompt('Type value1', 1);
-//         this.val2 = prompt('Type value2', 1);
-//     },
-//     sum(){
-//         let summ = +this.val1 + +this.val2;
-//         return 'summ = ' + summ;
-//     },
-//     mul(){
-//         let mull = +this.val1 * +this.val2;
-//         return  'mull = ' + mull;
-//     }
-// };
+var calculator = {
+    val1: '0',
+    val2: '0',
+    read(){
+        this.val1 = prompt('Type value1', 1);
+        this.val2 = prompt('Type value2', 1);
+    },
+    sum(){
+        let summ = +this.val1 + +this.val2;
+        return 'summ = ' + summ;
+    },
+    mul(){
+        let mull = +this.val1 * +this.val2;
+        return  'mull = ' + mull;
+    }
+};
 //
 // calculator.read();
-// alert( calculator.sum() );
-// alert( calculator.mul() );
+alert( calculator.sum() );
+alert( calculator.mul() );
 
 // chain-calls
-// var ladder = {
-//     step: 0,
-//     up: function() { // вверх по лестнице
-//         this.step++;
-//         return this
-//     },
-//     down: function() { // вниз по лестнице
-//         this.step--;
-//         return this
-//     },
-//     showStep: function() { // вывести текущую ступеньку
-//         alert( this.step );
-//         return this
-//     }
-// };
-// ladder.up().up().down().up().down().showStep(); // 1
+var ladder = {
+    step: 0,
+    up: function() { // вверх по лестнице
+        this.step++;
+        return this
+    },
+    down: function() { // вниз по лестнице
+        this.step--;
+        return this
+    },
+    showStep: function() { // вывести текущую ступеньку
+        alert( this.step );
+        return this
+    }
+};
+ladder.up().up().down().up().down().showStep(); // 1
 
 //two-one
-// let obj = {};
-// function A() {
-//     return obj
-// }
-// function B() {
-//     return obj
-// }
-//
-// var a = new A;
-// var b = new B;
-//
-// alert( a == b ); // true
+let obj = {};
+function A() {
+    return obj
+}
+function B() {
+    return obj
+}
+
+var a = new A;
+var b = new B;
+
+alert( a == b ); // true
 
 //calc-constructor
-// function Calculator() {
-//     this.val1= '0';
-//         this.val2= '0';
-//     this.read = () =>  {
-//         this.val1 = prompt('Type value1', 1);
-//         this.val2 = prompt('Type value2', 1);
-//     };
-//     this.sum = () =>  {
-//         return +this.val1 + +this.val2
-//     };
-//     this.mul = () => {
-//         return  +this.val1 * +this.val2
-//     };
-// }
-// var calculator = new Calculator();
-// calculator.read();
-//
-// alert( "Сумма=" + calculator.sum() );
-// alert( "Произведение=" + calculator.mul() );
+function Calculator() {
+    this.val1= '0';
+        this.val2= '0';
+    this.read = () =>  {
+        this.val1 = prompt('Type value1', 1);
+        this.val2 = prompt('Type value2', 1);
+    };
+    this.sum = () =>  {
+        return +this.val1 + +this.val2
+    };
+    this.mul = () => {
+        return  +this.val1 * +this.val2
+    };
+}
+var calculator = new Calculator();
+calculator.read();
 
-//accumulator
-// function Accumulator(startingValue){
-//     this.value = startingValue;
-//     this.read = function(){
-//         let addValue = prompt(`Текущее значение : ${this.value}
-// Введите число:`, 1);
-//         this.value += +addValue;
-//     }
-// }
+alert( "Сумма=" + calculator.sum() );
+alert( "Произведение=" + calculator.mul() );
+
+// accumulator
+function Accumulator(startingValue){
+    this.value = startingValue;
+    this.read = function(){
+        let addValue = prompt(`Текущее значение : ${this.value}
+Введите число:`, 1);
+        this.value += +addValue;
+    }
+}
 // var accumulator = new Accumulator(1); // начальное значение 1
 // accumulator.read(); // прибавит ввод prompt к текущему значению
 // accumulator.read(); // прибавит ввод prompt к текущему значению
 // alert( 'Результат = ' + accumulator.value ); // выведет текущее значение
 
-//calc extendable
-// function Calculator() {
-//     let action = {
-//         '-': function (a, b) {
-//             return +a - +b;
-//         },
-//         '+': function (a, b) {
-//             return +a + +b;
-//         }
-//     };
-//     this.calculate = function (val) {
-//         let a = val.split(' ')[0],
-//             b = val.split(' ')[2],
-//             sign = val.split(' ')[1]
-//         if(!isNaN(a) || !isNaN(b)){
-//             return action[sign] (a, b)
-//         } else {
-//             return 'error'
-//         }
-//     };
-//     this.addMethod = function(name, func){
-//         action[name] = func;
-//     };
-// }
-// var calc = new Calculator;
-//
-// var powerCalc = new Calculator;
-// powerCalc.addMethod("*", function(a, b) {
-//   return a * b;
-// });
-// powerCalc.addMethod("/", function(a, b) {
-//   return a / b;
-// });
-// powerCalc.addMethod("**", function(a, b) {
-//   return Math.pow(a, b);
-// });
-//
-// var result = powerCalc.calculate("2 ** 3");
-// alert( result ); // 8
-//
-// alert( calc.calculate("3 + 7") ); // 10
+// calc extendable
+function Calculator() {
+    let action = {
+        '-': function (a, b) {
+            return +a - +b;
+        },
+        '+': function (a, b) {
+            return +a + +b;
+        }
+    };
+    this.calculate = function (val) {
+        let a = val.split(' ')[0],
+            b = val.split(' ')[2],
+            sign = val.split(' ')[1]
+        if(!isNaN(a) || !isNaN(b)){
+            return action[sign] (a, b)
+        } else {
+            return 'error'
+        }
+    };
+    this.addMethod = function(name, func){
+        action[name] = func;
+    };
+}
+var calc = new Calculator;
+
+var powerCalc = new Calculator;
+powerCalc.addMethod("*", function(a, b) {
+  return a * b;
+});
+powerCalc.addMethod("/", function(a, b) {
+  return a / b;
+});
+powerCalc.addMethod("**", function(a, b) {
+  return Math.pow(a, b);
+});
+
+var result = powerCalc.calculate("2 ** 3");
+alert( result ); // 8
+
+alert( calc.calculate("3 + 7") ); // 10
 
 
 /* .. ваш код для filter, inBetween, inArray */
@@ -182,7 +182,6 @@ function filter(arr, func){
             someArr.push(elem);
         }
     })
-    console.log(someArr);
     return someArr;
 }
 function inBetween(a, b) {
@@ -196,33 +195,3 @@ alert(filter(arr, function(a) {
 })); // 2,4,6
 alert( filter(arr, inBetween(3, 6)) ); // 3,4,5,6
 alert( filter(arr, inArray([1, 2, 10])) ); // 1,2
-
-
-// var arr = [1, 2, 3, 4, 5, 6, 7];
-// function filter(arr, func) {
-//   var result = [];
-//
-//   for (var i = 0; i < arr.length; i++) {
-//     var val = arr[i];
-//     if (func(val)) {
-//       result.push(val);
-//     }
-//   }
-//
-//   return result;
-// }
-//
-// function inBetween(a, b) {
-//     return function(x) {
-//       return x >= a && x <= b;
-//     };
-//   }
-//
-// alert( filter(arr, inBetween(3, 6)) ); // 3,4,5,6
-// function inArray(arr) {
-//     return function(x) {
-//       return arr.indexOf(x) != -1;
-//     };
-//   }
-//
-// alert( filter(arr, inArray([1, 2, 10])) ); // 1,2
